c3b36fc577c35b412d1fbce0bdfa34c775434b96
Roberto Entringer <r.entringer@gmail.com>
2019-05-22 18:18:48 +0200

    Use parcel to create a bundler and implements draggable sortable

--- src/ntp.js
+++ src/ntp.js
@@ -1,7 +1,10 @@
-const bookmarkTreeNodes = chrome.bookmarks.getTree(bookmarkTreeNodes => {
-  const size = 24
+import Sortable from "@shopify/draggable/lib/sortable"
 
-  for (const item of bookmarkTreeNodes[0].children[0].children) {
+const init = () => chrome.bookmarks.getSubTree("1", subTree => {
+  const size = 24
+  const ntp = document.getElementById("ntp")
+
+  for (const item of subTree[0].children) {
     const url = `chrome://favicon/size/${size}@1x/${item.url}`
 
     const img = new Image()
@@ -20,14 +23,25 @@ const bookmarkTreeNodes = chrome.bookmarks.getTree(bookmarkTreeNodes => {
 
     const el = document.createElement("div")
     el.className = "item"
+    el.dataset.id = item.id
 
     el.appendChild(icon)
     el.appendChild(text)
 
     el.addEventListener("click", () => chrome.tabs.update({ url: item.url }))
 
-    window.app.appendChild(el)
+    ntp.appendChild(el)
   }
+
+  const sortable = new Sortable(ntp, { draggable: ".item" })
+
+  sortable.on("sortable:stop", sort => {
+    const items = sortable.getDraggableElementsForContainer(ntp)
+    for (let i = 0, size = items.length; i < size; i++)
+      chrome.bookmarks.move(items[i].dataset.id, { index: i })
+  })
 })
 
-document.addEventListener("DOMContentLoaded", bookmarkTreeNodes)
+document.addEventListener("DOMContentLoaded", init)
+
+//chrome.topSites.get(urls => console.log(urls))
