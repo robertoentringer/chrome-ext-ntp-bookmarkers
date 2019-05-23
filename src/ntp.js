5a5a0ce7bef63e9122e1b46b041f1091eb434760
Roberto Entringer <r.entringer@gmail.com>
2019-05-23 19:25:29 +0200

    Improvements  in ntp.js

--- src/ntp.js
+++ src/ntp.js
@@ -1,54 +1,51 @@
 import Sortable from "sortablejs"
 
-const ntp = document.getElementById("ntp")
-
 const dumpBookmarkers = subTree => {
-  const size = 24
-  for (const item of subTree[0].children) {
-    const url = `chrome://favicon/size/${size}@1x/${item.url}`
+  const iconSize = 24
+  const ntp = document.getElementById("ntp")
 
+  for (const bookmark of subTree[0].children) {
     const img = new Image()
-    img.src = url
-    img.height = size
-    img.width = size
-    img.alt = item.title
+    img.src = `chrome://favicon/size/${iconSize}@1x/${bookmark.url}`
+    img.height = iconSize
+    img.width = iconSize
+    img.alt = bookmark.title
 
     const icon = document.createElement("div")
     icon.appendChild(img)
     icon.className = "icon"
 
     const text = document.createElement("div")
-    text.textContent = item.title
+    text.textContent = bookmark.title
     text.className = "text"
 
     const el = document.createElement("div")
     el.className = "item"
-    el.dataset.id = item.id
+    el.dataset.id = bookmark.id
+    el.appendChild(icon)
+    el.appendChild(text)
+    el.addEventListener("click", () => chrome.tabs.update({ url: bookmark.url }))
     el.addEventListener("mouseenter", e => {
       if (!ntp.classList.contains("is-dragging")) e.target.classList.add("hover")
     })
     el.addEventListener("mouseleave", e => e.target.classList.remove("hover"))
 
-    el.appendChild(icon)
-    el.appendChild(text)
-    el.addEventListener("click", () => chrome.tabs.update({ url: item.url }))
-
     ntp.appendChild(el)
-    sortable.option("disabled", false)
+    sortable(ntp)
   }
 }
 
-const sortable = new Sortable(ntp, {
-  animation: 150,
-  disabled: true,
-  onStart(e) {
-    e.to.classList.add("is-dragging")
-  },
-  onEnd(e) {
-    e.to.classList.remove("is-dragging")
-    saveOrder(e.to.children)
-  },
-})
+const sortable = ntp =>
+  new Sortable(ntp, {
+    animation: 150,
+    onStart(e) {
+      e.to.classList.add("is-dragging")
+    },
+    onEnd(e) {
+      e.to.classList.remove("is-dragging")
+      saveOrder(e.to.children)
+    },
+  })
 
 const saveOrder = items => {
   for (let i = 0, size = items.length; i < size; i++)
