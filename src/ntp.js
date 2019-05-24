46db0d3b481d6ed7a75e49e3f31a2e097d39e64e
Roberto Entringer <r.entringer@gmail.com>
2019-05-24 15:40:48 +0200

    Implements sortable with a handler and code improvements

--- src/ntp.js
+++ src/ntp.js
@@ -1,8 +1,9 @@
 import Sortable from "sortablejs"
 
+const ntp = document.getElementById("ntp")
+
 const dumpBookmarkers = subTree => {
   const iconSize = 24
-  const ntp = document.getElementById("ntp")
 
   for (const bookmark of subTree[0].children) {
     const img = new Image()
@@ -10,6 +11,7 @@ const dumpBookmarkers = subTree => {
     img.height = iconSize
     img.width = iconSize
     img.alt = bookmark.title
+    img.draggable = false
 
     const icon = document.createElement("div")
     icon.appendChild(img)
@@ -19,24 +21,50 @@ const dumpBookmarkers = subTree => {
     text.textContent = bookmark.title
     text.className = "text"
 
-    const el = document.createElement("div")
+    const moveHandler = document.createElement("div")
+    moveHandler.className = "moveHandler"
+    moveHandler.innerHTML = "&vellip;"
+
+    const el = document.createElement("a")
     el.className = "item"
+    el.href = bookmark.url
     el.dataset.id = bookmark.id
     el.appendChild(icon)
     el.appendChild(text)
-    el.addEventListener("click", () => chrome.tabs.update({ url: bookmark.url }))
-    el.addEventListener("mouseenter", e => {
-      if (!ntp.classList.contains("is-dragging")) e.target.classList.add("hover")
-    })
-    el.addEventListener("mouseleave", e => e.target.classList.remove("hover"))
+    el.appendChild(moveHandler)
+
+    eventHandlers(el)
 
     ntp.appendChild(el)
     sortable(ntp)
   }
 }
 
+const eventHandlers = el => {
+  let showInfo
+
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    e.target.classList.remove("hover", "showInfo")
+    chrome.tabs.update({ url: el.href })
+  })
+
+  el.addEventListener("mouseenter", e => {
+    if (!ntp.classList.contains("is-dragging")) {
+      e.target.classList.add("hover")
+      showInfo = setTimeout(() => e.target.classList.add("showInfo"), 1000)
+    }
+  })
+
+  el.addEventListener("mouseleave", e => {
+    clearTimeout(showInfo)
+    e.target.classList.remove("hover", "showInfo")
+  })
+}
+
 const sortable = ntp =>
   new Sortable(ntp, {
+    handle: ".moveHandler",
     animation: 150,
     onStart(e) {
       e.to.classList.add("is-dragging")
@@ -44,7 +72,7 @@ const sortable = ntp =>
     onEnd(e) {
       e.to.classList.remove("is-dragging")
       saveOrder(e.to.children)
-    },
+    }
   })
 
 const saveOrder = items => {
